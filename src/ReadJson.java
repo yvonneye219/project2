41a22645bbc206aaea53e179c4d8391696976185
yvonneye <yvonne_ye26@milton.edu>
2025-12-10 08:50:10 -0500

    inclass work

--- src/ReadJson.java
+++ src/ReadJson.java
@@ -2,114 +2,127 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-
-// video to load jar
-//https://www.youtube.com/watch?v=QAJ09o3Xl_0
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-// Program for print data in JSON format.
-public class ReadJson {
-    public static void main(String args[]) throws ParseException {
-        // In java JSONObject is used to create JSON object
-        // which is a subclass of java.util.HashMap.
+import javax.swing.*;
+import java.awt.*;
 
+public class ReadJson {
+
+    public static void main(String args[]) throws Exception {
         JSONObject file = new JSONObject();
         file.put("Full Name", "Ritu Sharma");
         file.put("Roll No.", 1704310046);
         file.put("Tution Fees", 65400);
 
-
-        // To print in JSON format.
         System.out.print(file.get("Tution Fees"));
+
         ReadJson readingIsWhat = new ReadJson();
 
+        SwingUtilities.invokeLater(() -> {
+            PokemonViewer viewer = readingIsWhat.new PokemonViewer();
+            viewer.setVisible(true);
+        });
     }
 
-    public ReadJson(){
-        try {
-            pull();
-        }catch(Exception e){
-            System.out.println(e);
+    public ReadJson() {}
+
+    public JSONObject pull(String pokemonName) throws Exception {
+        String output;
+        String totalJson = "";
+
+        URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + pokemonName.toLowerCase());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+
+        if (conn.getResponseCode() != 200)
+            throw new RuntimeException("HTTP " + conn.getResponseCode());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        while ((output = br.readLine()) != null) {
+            totalJson += output;
         }
-    }
 
-    public  void pull() throws ParseException {
-        String output = "abc";
-        String totlaJson="";
-        try {
-
-            URL url = new URL("https://swapi.dev/api/people/4/");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            //if you get the error 403, uncomment line of code below
-            //conn.setRequestProperty("User-Agent", "Mozilla/5.0"); // Add User-Agent
-
-
-            if (conn.getResponseCode() != 200) {
-
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-                totlaJson+=output;
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        conn.disconnect();
 
         JSONParser parser = new JSONParser();
-        //System.out.println(str);
-        org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) parser.parse(totlaJson);
-        System.out.println(jsonObject);
+        return (JSONObject) parser.parse(totalJson);
+    }
 
-        try {
+    public class PokemonViewer extends JFrame {
 
-            String name = (String)jsonObject.get("name");
+        private JTextField nameField;
+        private JTextArea abilitiesArea;
+        private JTextField searchField;
+        private JButton searchButton;
 
+        public PokemonViewer() {
+            setTitle("Pokemon Viewer");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(450, 350);
+            setLocationRelativeTo(null);
 
+            setLayout(new BorderLayout(10, 10));
 
-            org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObject.get("films");
-            int n =   msg.size(); //(msg).length();
-            for (int i = 0; i < n; ++i) {
-                String test =(String) msg.get(i);
-                System.out.println(test);
-                // System.out.println(person.getInt("key"));
-            }
-            String height= (String)jsonObject.get("height");
-            String mass   = (String)jsonObject.get("mass");
-            System.out.println(name);
-            System.out.println(height);
-            System.out.println(mass);
+            JPanel topPanel = new JPanel(new GridLayout(2, 1, 5, 5));
+
+            nameField = new JTextField();
+            nameField.setEditable(false);
+            nameField.setBorder(BorderFactory.createTitledBorder("Name"));
+
+            abilitiesArea = new JTextArea(3, 20);
+            abilitiesArea.setEditable(false);
+            abilitiesArea.setLineWrap(true);
+            abilitiesArea.setWrapStyleWord(true);
+            JScrollPane abilitiesScroll = new JScrollPane(abilitiesArea);
+            abilitiesScroll.setBorder(BorderFactory.createTitledBorder("Abilities"));
+
+            topPanel.add(nameField);
+            topPanel.add(abilitiesScroll);
+
+            add(topPanel, BorderLayout.NORTH);
+
+            add(new JPanel(), BorderLayout.CENTER);
+
+            JPanel bottomPanel = new JPanel(new BorderLayout(5, 5));
+
+            searchField = new JTextField();
+            searchField.setBorder(BorderFactory.createTitledBorder("Pokemon Name"));
+            bottomPanel.add(searchField, BorderLayout.CENTER);
+
+            searchButton = new JButton("Search");
+            bottomPanel.add(searchButton, BorderLayout.EAST);
+
+            add(bottomPanel, BorderLayout.SOUTH);
+
+            searchButton.addActionListener(event -> {
+                try {
+                    String pokemonName = searchField.getText().trim();
+                    JSONObject jsonObject = ReadJson.this.pull(pokemonName);
+
+                    String name = (String) jsonObject.get("name");
+                    nameField.setText(name);
+
+                    JSONArray abilities = (JSONArray) jsonObject.get("abilities");
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < abilities.size(); i++) {
+                        JSONObject abilityObject = (JSONObject) abilities.get(i);
+                        JSONObject abilityInfo = (JSONObject) abilityObject.get("ability");
+                        sb.append("-").append(abilityInfo.get("name")).append("\n");
+                    }
+
+                    abilitiesArea.setText(sb.toString());
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
         }
-
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-
     }
 }
-
-
